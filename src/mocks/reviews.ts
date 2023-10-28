8a13bb5d5e895307f30a5771ed90fe6b9b7caa10
Arina0212 <Shubina.Arina@urfu.me>
2023-10-28 19:41:25 +0500

    Больше подробностей(часть 1)

--- src/mocks/reviews.ts
+++ src/mocks/reviews.ts
@@ -11,21 +11,21 @@ export type Reviews = Review[];
 export const reviews: Review[] = [
   {
     FilmId:'',
-    Comment: "Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed films in years.",
+    Comment: 'Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director"s funniest and most exquisitely designed films in years.',
     Author: 'Kate Muir',
     PubDate: 'December 24, 2016',
     Rating: '8,9',
   },
   {
     FilmId:'',
-    Comment: "Anderson's films are too precious for some, but for those of us willing to lose ourselves in them, they're a delight. 'The Grand Budapest Hotel' is no different, except that he has added a hint of gravitas to the mix, improving the recipe.",
+    Comment: 'Anderson"s films are too precious for some, but for those of us willing to lose ourselves in them, they"re a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.',
     Author: 'Bill Goodykoontz',
     PubDate: 'November 18, 2015',
     Rating: '8,0',
   },
   {
     FilmId:'',
-    Comment: "I didn't find it amusing, and while I can appreciate the creativity, it's an hour and 40 minutes I wish I could take back.",
+    Comment: 'I didn"t find it amusing, and while I can appreciate the creativity, it"s an hour and 40 minutes I wish I could take back.',
     Author: 'Amanda Greever',
     PubDate: 'November 18, 2015',
     Rating: '8,0',
