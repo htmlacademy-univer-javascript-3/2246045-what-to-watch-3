3637be9c2a67a6da0b1cc214ffba94a4a5b39c25
Arina0212 <Shubina.Arina@urfu.me>
2023-11-29 15:05:34 +0500

    доработка2

--- src/components/pages/main-page/film-list-props.ts
+++
@@ -1,9 +0,0 @@
-export type FilmSmall = {
-    id: string;
-    name: string;
-    previewImage: string;
-    previewVideoLink: string;
-    genre: string;
-}
-
-export type Films = FilmSmall[];
--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App, { AppProps } from './components/app/app';
 import { films } from './mocks/films';
-import { promoFilm } from './mocks/promo-film';
+import { promoFilm } from './mocks/promo';
 import { PreviewFilm } from './types/preview-film';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { checkAuthAction, fetchFilmsAction } from './store/api-actions';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import ReactDOM from 'react-dom';
-import React from 'react';
 store.dispatch(fetchFilmsAction());
 store.dispatch(checkAuthAction());
-
+const appData: AppProps = {
+  promoFilmCard: promoFilm,
+  smallFilmCards: films.slice(1, films.length) as PreviewFilm[],
+  films: []
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ToastContainer />
-      <App />
+      <App
+        promoFilmCard={appData.promoFilmCard}
+        smallFilmCards={appData.smallFilmCards}
+        films={appData.films}
+      />
     </Provider>
   </React.StrictMode>
 );
